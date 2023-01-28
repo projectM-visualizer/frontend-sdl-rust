5a23b075081aa65694a084689993f9563d2e2f25
Ryan Kois <ryan.kois@gmail.com>
2023-01-28 11:34:21 -0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
+use projectm_rs::core::*;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use projectm_rs::core::*;
+use sdl2::video::GLProfile;
+
+fn find_sdl_gl_driver() -> Option<u32> {
+    for (index, item) in sdl2::render::drivers().enumerate() {
+        if item.name == "opengl" {
+            return Some(index as u32);
+        }
+    }
+    None
+}
 
 fn main() -> Result<(), String> {
     // setup sdl
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
-    // let audio_subsystem = sdl_context.audio()?;
+
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(GLProfile::Core);
+    gl_attr.set_context_version(3, 3);
 
     // create window
     // get screen dimensions
@@ -14,13 +27,18 @@ fn main() -> Result<(), String> {
     let display_mode = video_subsystem.desktop_display_mode(display_index)?;
     let mut window_width = display_mode.w as u32;
     let mut window_height = display_mode.h as u32;
-    let window = video_subsystem.window("frontend-sdl2-rust",  window_width, window_height)
+    let window = video_subsystem
+        .window("frontend-sdl2-rust", window_width, window_height)
+        .opengl()
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
-    
+
     // create canvas/renderer
-    let mut canvas = window.into_canvas().build()
+    let mut canvas = window
+        .into_canvas()
+        .index(find_sdl_gl_driver().unwrap())
+        .build()
         .expect("could not make a canvas");
 
 
