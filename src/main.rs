cbf2ae5a5004cc14f40c43a4e6c1a1132908870b
Mischa Spiegelmock <me@mish.dev>
2023-01-28 22:49:15 +0200

    Initialize GL context (#1)
    * wip
    
    * working on macOS
    
    ---------
    
    Co-authored-by: Ryan Kois <ryan.kois@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,47 @@
+use projectm_rs::core::*;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use projectm_rs::core::*;
+use sdl2::video::GLProfile;
+
 
 fn main() -> Result<(), String> {
     // setup sdl
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
-    // let audio_subsystem = sdl_context.audio()?;
+
+    // request GL version
+    // TODO: deal with OpenGL ES here
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(GLProfile::Core);
+    gl_attr.set_context_version(3, 3);
+    gl_attr.set_context_flags().debug().set();
+    assert_eq!(gl_attr.context_profile(), GLProfile::Core);
+    assert_eq!(gl_attr.context_version(), (3, 3));
 
     // create window
     // get screen dimensions
-    let mut display_index = 0;
+    let  display_index = 0;
     let display_mode = video_subsystem.desktop_display_mode(display_index)?;
-    let mut window_width = display_mode.w as u32;
-    let mut window_height = display_mode.h as u32;
-    let window = video_subsystem.window("frontend-sdl2-rust",  window_width, window_height)
+    let  window_width = display_mode.w as u32;
+    let  window_height = display_mode.h as u32;
+    let window = video_subsystem
+        .window("frontend-sdl2-rust", window_width, window_height)
+        .opengl()
         .position_centered()
+        .allow_highdpi()
         .build()
         .expect("could not initialize video subsystem");
-    
-    // create canvas/renderer
-    let mut canvas = window.into_canvas().build()
-        .expect("could not make a canvas");
 
+    // create openGL context
+    let gl_context = window.gl_create_context().unwrap();
+    window.gl_make_current(&gl_context).unwrap();
 
-    // projectm::init
-    let projectm_handle = unsafe {
-        projectm::create()
-    };
+    // initialize projectM
+    let projectm_handle = projectm::create() ;
 
-    unsafe {
-        projectm::set_window_size(projectm_handle, canvas.output_size().unwrap().0.try_into().unwrap(), canvas.output_size().unwrap().1.try_into().unwrap())
-    }
-    println!("projectm initialized!");
+    // get/set window size
+    let (width, height) = window.drawable_size(); // highDPI aware
+    projectm::set_window_size(projectm_handle, width.try_into().unwrap(), height.try_into().unwrap());
 
     // events
     let mut event_pump = sdl_context.event_pump()?;
@@ -54,13 +63,11 @@ fn main() -> Result<(), String> {
         // generate random audio
         generate_random_audio_data(projectm_handle);
 
-        // projectm::render
-        unsafe {
+        // render a frame
             projectm::render_frame(projectm_handle);
-        }
         
-        // present/render
-        canvas.present();
+        // swap buffers
+        window.gl_swap_window();
     }
 
     // finish okay
@@ -83,7 +90,5 @@ fn generate_random_audio_data(projectm_handle: projectm_handle)
         i += 1
     };
 
-    unsafe {
         projectm::pcm_add_int16(projectm_handle, &pcm_data[0][0], 512, 2)
-    }
 }
