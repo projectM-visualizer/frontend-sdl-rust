5227fa4351c75160b89ffbb8e223dbc944aa1335
Mischa Spiegelmock <me@mish.dev>
2023-10-07 20:33:28 -0700

    basic config loading

--- src/app/playlist.rs
+++ src/app/playlist.rs
@@ -1,10 +1,12 @@
+use std::path::PathBuf;
+
 use crate::app::App;
 
 impl App {
     /// Add presets to the playlist recursively skipping duplicates.
-    pub fn add_preset_path(&self, preset_path: &str) {
-        self.playlist.add_path(preset_path, true);
-        println!("added preset path: {}", preset_path);
+    pub fn add_preset_path(&self, preset_path: &PathBuf) {
+        self.playlist.add_path(preset_path.to_str().unwrap(), true);
+        println!("added preset path: {}", preset_path.to_str().unwrap());
         println!("playlist size: {}", self.playlist.len());
     }
 
