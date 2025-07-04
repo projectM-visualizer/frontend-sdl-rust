814c0e060b295fe64bb8a5d97f6d0fd020fb251c
mintybasil <163682877+mintybasil@users.noreply.github.com>
2025-07-04 19:42:37 -0400

    Expands cli flags, adds env vars

--- src/main.rs
+++ src/main.rs
@@ -12,73 +12,55 @@ use confique::Config as ConfiqueConfig;
 ///
 /// Need help? Join discord: https://discord.gg/uSSggaMBrv
 struct Cli {
-    // /// Get help
-    // #[clap(short, long)]
-    // help: bool,
-    /// Path to preset directory
-    #[clap(short, long)]
-    preset_path: Option<PathBuf>,
-
-    /// Path to texture directory
-    #[clap(short, long)]
-    texture_path: Option<PathBuf>,
-
+    #[arg(short, long)]
     /// Path to config file
-    #[clap(short, long)]
     config_path: Option<PathBuf>,
 
+    #[arg(short, long)]
+    #[arg(default_value = "60")]
+    #[arg(env = "PM_FRAME_RATE")]
+    frame_rate: Option<u32>,
+
+    #[arg(short, long)]
+    #[arg(env = "PM_PRESET_PATH")]
+    /// Path to preset directory
+    preset_path: Option<PathBuf>,
+
+    #[arg(short, long)]
+    #[arg(env = "PM_TEXTURE_PATH")]
+    /// Path to texture directory
+    texture_path: Option<PathBuf>,
+
+    #[arg(short, long)]
+    #[arg(default_value = "1.0")]
+    #[arg(env = "PM_BEAT_SENSITIVITY")]
+    beat_sensitivity: Option<f32>,
+
+    #[arg(short = 'd', long)]
+    #[arg(default_value = "10")]
+    #[arg(env = "PM_PRESET_DURATION")]
+    preset_duration: Option<f64>,
+
+    #[arg(short, long)]
+    #[arg(env = "PM_AUDIO_INPUT")]
     /// Audio input device (name or index)
-    #[clap(short, long)]
     audio_input: Option<String>,
 }
 
 fn main() -> Result<(), String> {
-    // parse command line arguments
-    let cli = Cli::parse();
-
-    // show help
-    // cli.help.then(|| usage());
-
-    // Initialize AppConfig with default values
-    let mut app_config = Config::default();
-
-    // Load from environment and config files using confique
-    let file_config = match Config::builder()
-        .env()
-        .file(
-            cli.config_path
-                .as_ref()
-                .unwrap_or(&PathBuf::from("conf.toml")),
-        )
-        .file("conf.yaml")
-        .file("conf.json")
-        // .file("/usr/local/share/projectM/config.inp")  TODO: custom parser
-        .load()
-    {
-        Ok(config) => config,
-        Err(e) => {
-            panic!("Error loading config: {}", e);
-        }
+    let cli_config = Cli::parse();
+    let app_config = Config {
+        frame_rate: cli_config.frame_rate,
+        preset_path: cli_config.preset_path,
+        texture_path: cli_config.texture_path,
+        beat_sensitivity: cli_config.beat_sensitivity,
+        preset_duration: cli_config.preset_duration,
     };
-
-    // Merge the loaded configuration into the default one
-    app_config.merge(file_config);
-
-    // Override with CLI arguments
-    if let Some(preset_path) = cli.preset_path {
-        app_config.preset_path = Some(preset_path);
-    }
-    if let Some(texture_path) = cli.texture_path {
-        app_config.texture_path = Some(texture_path);
-    }
-    // Add other CLI overrides as needed
 
     // Initialize the application
     let mut app = app::App::new(Some(app_config));
     app.init();
     app.main_loop();
-
-
 
     Ok(())
 }
